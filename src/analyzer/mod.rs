6da75cb678bdfccdb737dfc4f3119909224c62db
Manas <manas18244@iiitd.ac.in>
2024-07-23 19:50:19 +0530

    Add assertions to cover analyzer mod

--- src/analyzer/mod.rs
+++ src/analyzer/mod.rs
@@ -3,19 +3,26 @@ pub mod config;
 
 #[cfg(test)]
 mod tests {
-    use crate::error::Result;
+    use crate::error::{QccErrorKind, Result};
     use crate::parser::Parser;
+    use crate::{assert_eq_all, assert_eq_any};
 
     #[test]
     fn check_analyzer() -> Result<()> {
         let path = "tests/test1.ql".into();
         let args = vec![path];
-        let mut parser = Parser::new(args)?.unwrap();
+
+        let parser = Parser::new(args);
+        assert!(parser.is_ok());
+        let parser = parser.unwrap();
+        assert!(parser.is_some());
+        let mut parser = parser.unwrap();
 
         let config = parser.get_config();
-        let qast = parser.parse(&config.analyzer.src)?;
-        println!("{qast}");
-        config.analyzer.analyze(&qast)?;
+        let qast = parser.parse(&config.analyzer.src);
+        assert!(qast.is_ok());
+        let qast = qast.unwrap();
+        assert!(config.analyzer.analyze(&qast).is_ok());
 
         Ok(())
     }
